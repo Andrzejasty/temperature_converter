7c042899685b7aef60f39a8ba0311e385fd7c542
Andrzejasty <aszpit@gmail.com>
2020-07-18 16:28:14 +0200

    commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double celsiusDegre;
+	Scanner input = new Scanner(System.in);
+	System.out.println("Podaj stopnie w C: ");
+    celsiusDegre = input.nextDouble();
+    System.out.println("Wynik: "+(celsiusDegre*1.8 + 32));
+    }
+    
+}
